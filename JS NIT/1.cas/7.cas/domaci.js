faf67c213f70a858a5477bba4276e9093e41e57e
DzedajMuharem <mujanovich100@gmail.com>
2022-10-30 17:25:30 +0100

    7.cas

---
+++ JS NIT/1.cas/7.cas/domaci.js
@@ -0,0 +1,8 @@
+console.log(new Date().getDay());
+// getDay() - daje vrijednost od 0-6
+
+// switch(new Date().getDay());
+// Na osnovu vrijednosti koju nam daje izraz iz swtich naredbe kroz caseove
+// i eventualn defauta ispsiati poruku
+// 'Danas je 'radni dan''
+// 'Ugodno provedite vikend'
