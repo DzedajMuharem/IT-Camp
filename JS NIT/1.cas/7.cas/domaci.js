2a2563308db88e67ddbca014672bf3d3f35ebfdf
DzedajMuharem <mujanovich100@gmail.com>
2022-10-31 17:55:36 +0100

    forloop+domaci7.cas

--- JS NIT/1.cas/7.cas/domaci.js
+++ JS NIT/1.cas/7.cas/domaci.js
@@ -1,4 +1,4 @@
-console.log(new Date().getDay());
+// console.log(new Date().getDay());
 // getDay() - daje vrijednost od 0-6
 
 // switch(new Date().getDay());
@@ -6,27 +6,29 @@ console.log(new Date().getDay());
 // i eventualn defauta ispsiati poruku
 // 'Danas je 'radni dan''
 // 'Ugodno provedite vikend'
-
-switch (new Date().getDay()) {
-  case 0:
-    console.log("Ugodno provedite vikend");
-    break;
-  case 1:
-    console.log("Danas je radni dan");
-    break;
-  case 2:
-    console.log("Danas je radni dan");
-    break;
-  case 3:
-    console.log("Danas je radni dan");
-    break;
-  case 4:
-    console.log("Danas je radni dan");
-    break;
-  case 5:
-    console.log("Danas je radni dan");
-    break;
-  case 6:
-    console.log("Ugodno provedite vikend");
-    break;
+let a = new Date().getDay();
+for (a = 0; a < 7; a++) {
+  switch (a) {
+    case 0:
+      console.log("Ugodno provedite vikend");
+      break;
+    case 1:
+      console.log("Danas je radni dan");
+      break;
+    case 2:
+      console.log("Danas je radni dan");
+      break;
+    case 3:
+      console.log("Danas je radni dan");
+      break;
+    case 4:
+      console.log("Danas je radni dan");
+      break;
+    case 5:
+      console.log("Danas je radni dan");
+      break;
+    case 6:
+      console.log("Ugodno provedite vikend");
+      break;
+  }
 }
