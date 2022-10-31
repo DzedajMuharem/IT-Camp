7f0b8100af87575443287ec41db8c946c70db911
DzedajMuharem <mujanovich100@gmail.com>
2022-10-31 17:37:58 +0100

    jsvezbanja

---
+++ JS vezbanja/app.js
@@ -0,0 +1,47 @@
+// 1.
+//  Write a JavaScript for loop that will iterate from 0 to 15.
+// For each iteration, it will check if the current number is odd or even,
+// and display a message to the screen.
+
+// for (let a = 0; a < 16; a++) {
+//   if (a % 2 === 0) {
+//     console.log(a + " paran");
+//   } else if (a % 2 === 1) {
+//     console.log(a + " neparan");
+//   }
+// }
+
+// ------------------------------------------------ //
+
+// 2.
+// Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three
+// print "Fizz" instead of the number and for the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
+// for (let b = 1; b < 101; b++) {
+//   if (b % 3 === 0 && b % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (b % 3 === 0) {
+//     console.log("fizz");
+//   } else if (b % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(b);
+//   }
+// }
+
+// ------------------------------------------------ //
+
+// 3.
+// Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+
+// let zbirc = 0;
+// for (let c = 0; c < 1001; c++) {
+//   if (c % 3 === 0 && c % 5 === 0) {
+//     zbirc += c;
+//   }
+// }
+// console.log(zbirc);
+
+// 4.
+// Write the java statement that assigns 1 to x if y is greater than 0
