d0e94198f9989755654e0f5bbcad8e878ed48fdd
DzedajMuharem <mujanovich100@gmail.com>
2022-10-31 17:48:39 +0100

    domaci7.cas

--- JS NIT/1.cas/7.cas/domaci.js
+++ JS NIT/1.cas/7.cas/domaci.js
@@ -6,3 +6,27 @@ console.log(new Date().getDay());
 // i eventualn defauta ispsiati poruku
 // 'Danas je 'radni dan''
 // 'Ugodno provedite vikend'
+
+switch (new Date().getDay()) {
+  case 0:
+    console.log("Ugodno provedite vikend");
+    break;
+  case 1:
+    console.log("Danas je radni dan");
+    break;
+  case 2:
+    console.log("Danas je radni dan");
+    break;
+  case 3:
+    console.log("Danas je radni dan");
+    break;
+  case 4:
+    console.log("Danas je radni dan");
+    break;
+  case 5:
+    console.log("Danas je radni dan");
+    break;
+  case 6:
+    console.log("Ugodno provedite vikend");
+    break;
+}
