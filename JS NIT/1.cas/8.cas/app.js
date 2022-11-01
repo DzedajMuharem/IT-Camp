146116e581fc4c85fb30f3eed725c9c20bf5326c
DzedajMuharem <mujanovich100@gmail.com>
2022-11-01 20:59:52 +0100

    8.cas

---
+++ JS NIT/1.cas/8.cas/app.js
@@ -0,0 +1,70 @@
+// 1.
+// Prebrojati i sabrati brojeve deljive sa 5 u intervalu 1 do n
+
+// let broj1 = +prompt("Unesite broj");
+// let suma = 0;
+// let brojac = 0;
+// for (let a = 1; a <= broj1; a++) {
+//   if (a % 5 === 0) {
+//     // console.log(a);
+//     suma += a;
+//     brojac++;
+//   }
+// }
+// console.log(
+//   "Zbir svih brojeva deljivih sa 5 iznosi: " + suma + ", i ima ih: " + brojac
+// );
+
+// 2.
+// Neka se izvrsi iteracija od broja 99 do - 99
+// Ispisati zbir broja deljivog sa 4 i 14.
+
+// let zbir = 0;
+// for (let b = 99; b >= -99; b--) {
+//   if (b % 4 === 0) {
+//     console.log(b + 14);
+//   }
+// }
+
+// console.log(zbir);
+// let zbiec = 0;
+// let c = 99;
+// while (c >= -99) {
+//   if (c % 4 === 0) {
+//     console.log(c + 14);
+//   }
+//   c--;
+// }
+
+// 3.
+// Izracunati aritmeticku sredinu brojeva koji su deljivi sa 3.
+// Iteracija se vrsi od 3 do 17
+
+// let zbir = 0;
+// let brojacd = 0;
+
+// for (let d = 3; d <= 17; d++) {
+//   if (d % 3 === 0) {
+//     // console.log(d);
+//     zbir += d;
+//     brojacd++;
+//   }
+// }
+// console.log("Aritmeticka sredina iznosi " + zbir / brojacd);
+
+// let sumad = 0;
+// let brojacdd = 0;
+
+// let dd = 3;
+
+// while (dd <= 17) {
+//   if (dd % 3 === 0) {
+//     sumad += dd;
+//     brojacdd++;
+//   }
+//   dd++;
+// }
+// console.log("Aritmeticka sredina iznosi: " + sumad / brojacdd);
+
+// 4.
+// Fudbalski teren dimenzija d*s treba ograditi pravougaonom ogradom tako
