020725326e5a9fc3bc553dca76676018854b2c4d
DzedajMuharem <mujanovich100@gmail.com>
2022-11-02 20:46:01 +0100

    8.cas

--- JS NIT/1.cas/8.cas/app.js
+++ JS NIT/1.cas/8.cas/app.js
@@ -67,4 +67,51 @@
 // console.log("Aritmeticka sredina iznosi: " + sumad / brojacdd);
 
 // 4.
-// Fudbalski teren dimenzija d*s treba ograditi pravougaonom ogradom tako
+// Задатак: Фудбалски терен
+// Фудбалски терен димензија d×s треба оградити правоугаоном оградом тако да је растојање страница ограде
+// од линије терена r. Напиши програм који одређује дужину ограде.
+// Улаз: У три реда стандардног улаза налазе се три цела броја:
+// • d - дужина терена у метрима (90 ≤ d ≤ 120)
+// • s - ширина терена у метрима (45 ≤ s ≤ 90)
+// • r - растојање ограде од терена у метрима (2 ≤ r ≤ 10)
+
+let d = +prompt(
+  "Unesite duzinu terena u metrima (od 90 do 120 ukljucujuci ta dva broja)"
+);
+let s = +prompt(
+  "Unesite sirinu terena u metrima (od 45 do 90, ukljucujuci ta dva broja)"
+);
+let r = +prompt(
+  "Unesite rastojanje ograde od terena u metrima(od 2 do 10, ukljucujuci ta dva broja)"
+);
+let duzinaOgrade = 0;
+
+for (let g = 90; g <= 120; g++) {
+  if (d <= 89 || d >= 121) {
+    console.log("Niste uneli korektne vrijednosti za d");
+    break;
+  } else if (s <= 44 || s >= 91) {
+    console.log("Niste uneli korektne vrijednosti za s");
+    break;
+  } else if (r <= 1 || r >= 11) {
+    console.log("Niste uneli korektne vrijednosti za r");
+    break;
+  } else if (isNaN(d) || isNaN(s) || isNaN(r)) {
+    console.log("Uneli ste nesto sto nije broj");
+    break;
+  } else if (g === d) {
+    duzinaOgrade = 2 * g + 2 * s + 8 * r;
+  }
+}
+console.log("Duzina ograde iznosi: " + duzinaOgrade + "m");
+
+// Domaci zadaci:
+// 1.
+// Korisnik unosi 2 broja:
+// Na osnovu toga koji je broj manji, iteracija se vrsi od njega do veceg broja.
+// Izracunati aritmeticku sredinu brojeva koji su deljivi sa 5.
+
+// 2.
+// Korisnik unosi broj iz intervala [12,16).
+// Na osnovu unetog broja, iteracija se vrsi od njega do 1(ukljucujuci).
+// Ispisati svaki broj iz iteracije, nljegov kvadrat i vrednost broja umanjenu za 25.
