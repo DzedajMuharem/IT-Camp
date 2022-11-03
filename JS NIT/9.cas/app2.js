ec8fdf8ec17dd6b9d753faa9245cb945cfe0f8ae
DzedajMuharem <mujanovich100@gmail.com>
2022-11-03 21:01:53 +0100

    9.cas

---
+++ JS NIT/9.cas/app2.js
@@ -0,0 +1,32 @@
+let recenica = "Danas cemo raditi neke operacije sa stringovima";
+console.log(recenica);
+// console.log(typeof recenica);
+
+let rec = "Omar";
+
+// Pristupanje nekom karakteru stringa se vrsi preko indexa.
+// Indeksiranje ide od nule.
+// U svakom iterirajucem objektu, poslednji karakter ima index:
+// (Ukupna duzina stringa) -1
+
+console.log(rec[0]);
+console.log(rec[2]);
+
+// Metoda za dobijanje ukupne duzine stringa je length:
+let duzinaRecenice = recenica.length;
+console.log(duzinaRecenice);
+console.log(rec.length);
+
+// Primer
+// Svaki karakter neke recenice neka se ispise jedan ispod drugog.
+
+// Kod ovakvih zadataka iterator predstavlja index pomocu kojeg pristupamo karakteru stringa.
+
+recenica = "Danas cemo raditi neke operacije sa stringovima";
+for (let i = 0; i < recenica.length; i++) {
+  console.log(recenica[i]);
+}
+
+// Na osnovu unete recenice od stane korisnika ispisati recenicu tako da svaki karakter ide
+// jedan ispod drugog. Dok ako je neki karakter jednak = 'a', neka se ispise broj 5.
+// A u slucaju da je karakter jednak 's', neka se ne ispise nista
