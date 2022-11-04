9330efb58bd1d2afb64a7b778b023eff926cc669
DzedajMuharem <mujanovich100@gmail.com>
2022-11-04 22:24:31 +0100

    9.cas

--- JS NIT/9.cas/app2.js
+++ JS NIT/9.cas/app2.js
@@ -22,11 +22,24 @@ console.log(rec.length);
 
 // Kod ovakvih zadataka iterator predstavlja index pomocu kojeg pristupamo karakteru stringa.
 
-recenica = "Danas cemo raditi neke operacije sa stringovima";
-for (let i = 0; i < recenica.length; i++) {
-  console.log(recenica[i]);
-}
+// recenica = "Danas cemo raditi neke operacije sa stringovima";
+// for (let i = 0; i < recenica.length; i++) {
+//   console.log(recenica[i]);
+// }
 
 // Na osnovu unete recenice od stane korisnika ispisati recenicu tako da svaki karakter ide
 // jedan ispod drugog. Dok ako je neki karakter jednak = 'a', neka se ispise broj 5.
 // A u slucaju da je karakter jednak 's', neka se ne ispise nista
+
+let recenica2 = prompt("Ispisite recenicu");
+
+for (let i = 0; i < recenica2.length; i++) {
+  // console.log(recenica2[i]);
+  if (recenica2[i] === "a") {
+    console.log(5);
+  } else if (recenica2[i] === "s") {
+    continue;
+  } else {
+    console.log(recenica2[i]);
+  }
+}
