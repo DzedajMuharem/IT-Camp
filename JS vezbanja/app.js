5dfa5327b26b3b104c6bb0886d11dd4f69906840
DzedajMuharem <mujanovich100@gmail.com>
2022-11-07 09:01:43 +0100

    vezbanje

--- JS vezbanja/app.js
+++ JS vezbanja/app.js
@@ -136,3 +136,14 @@
 // Write a JavaScript function to repeat a string a specified times.
 
 // ------------------------------------------------- //
+
+// 11.
+// Write a JavaScript function to convert a string in abbreviated form.
+abbrev_name = function (str1) {
+  var split_names = str1.trim().split(" ");
+  if (split_names.length > 1) {
+    return split_names[0] + " " + split_names[1].charAt(0) + ".";
+  }
+  return split_names[0];
+};
+console.log(abbrev_name("Robin Singh"));
