0ebf70836f21fcf8a2ad9cbda4035f783caa09fc
DzedajMuharem <mujanovich100@gmail.com>
2022-12-30 17:31:49 +0100

    domaci 29.cas

--- JS NIT/29.cas/zadatak.js
+++ JS NIT/29.cas/zadatak.js
@@ -3,9 +3,7 @@
 
 const zadatak1 = () => {
   let danasnji = new Date();
-  console.log(danasnji.toLocaleString());
   let sati = danasnji.getHours();
-  console.log(sati);
   let minut = danasnji.getMinutes();
   let sekund = danasnji.getSeconds();
   let praviSati;
@@ -22,3 +20,28 @@ const zadatak1 = () => {
   return `Trenutno vrijeme je: ${praviSati} : ${minut} : ${sekund}`;
 };
 console.log(zadatak1());
+
+// Write a JavaScript program to find 1st january is being a Sunday between 2014 and 2050.
+
+const zadatak2 = () => {
+  let prvi = new Date(2014, 00, 01);
+  let drugi = new Date(2050, 00, 01);
+  let godinaPrvi = prvi.getFullYear();
+  let godinaDrugi = drugi.getFullYear();
+
+  let brojac = 0;
+
+  for (let i = godinaPrvi; i <= godinaDrugi; i++) {
+    let noviDatum = new Date(i, 00, 01);
+    if (noviDatum.getDay() === 0) {
+      console.log(i);
+      brojac++;
+    } else {
+      continue;
+    }
+  }
+
+  console.log(brojac);
+  return `1. Januar pada u nedelju ${brojac} puta.`;
+};
+console.log(zadatak2());
