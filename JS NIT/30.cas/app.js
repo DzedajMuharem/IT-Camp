b13ff9a83a5383e46984d104ae37ea8af01bdc0b
DzedajMuharem <mujanovich100@gmail.com>
2023-01-08 21:35:49 +0100

    32.cas

--- JS NIT/30.cas/app.js
+++ JS NIT/30.cas/app.js
@@ -1,7 +1,7 @@
 // Niz predstavlja strukturu podataka koja moze sadrzati vise od jedne vrijednosti
 // Nizovi u JS su heterogeni (mogu sadrzati elemente razlicitih tipova)
 
-const niz = ["Mitar", "Vranic", 20, "punoletan", true];
+const niz = ['Mitar', 'Vranic', 20, 'punoletan', true];
 console.log(niz);
 
 // Dozvoljeno je, cesto i prakticnije pisati svaki element niza u novom redu
@@ -17,23 +17,23 @@ console.log(niz);
 // Mozemo prvo kreirati  prazan niz pa nakon toga dodavati rucno elemente
 
 const automobili = [];
-automobili[0] = "Audi";
+automobili[0] = 'Audi';
 console.log(automobili);
-automobili[1] = "Passerati";
+automobili[1] = 'Passerati';
 console.log(automobili);
-automobili[5] = "Golf";
+automobili[5] = 'Golf';
 console.log(automobili);
 
 console.log(automobili[3]);
 
 // Istu sintaksu mozemo iskoristiti za promeni vrijednosti elementa
 
-automobili[0] = "Mercedes";
+automobili[0] = 'Mercedes';
 console.log(automobili);
 
 // Niz mozemo definisati preko new keyworda
 
-const niz3 = new Array("Hasan", "Omar", "Emin");
+const niz3 = new Array('Hasan', 'Omar', 'Emin');
 console.log(niz3);
 
 // Radi jednostavnosti, citljivosti koda, i brzeg izvrsavanja je bolje koristit [] nego new keyword.
@@ -51,7 +51,7 @@ console.log(typeof automobili);
 
 // 1. Array.isArray(potencijalnoNiz)
 console.log(Array.isArray(automobili));
-console.log(Array.isArray("string"));
+console.log(Array.isArray('string'));
 
 // 2. instanceof Array (potencijalnoNiz)
 console.log(automobili instanceof Array);
@@ -64,14 +64,14 @@ console.log(automobili.length);
 
 // Za dodavanje elementa na kraju niza se koristi push() metoda
 
-const dani = ["Ponedeljak", "Utorak"];
-dani.push("Srijeda");
+const dani = ['Ponedeljak', 'Utorak'];
+dani.push('Srijeda');
 console.log(dani);
 // dani.push(...) vraca novu duzina=u niza
-console.log(dani.push("Cetvrtak", "Petak"));
+console.log(dani.push('Cetvrtak', 'Petak'));
 console.log(dani);
 
-dani[dani.length] = "Subota";
+dani[dani.length] = 'Subota';
 console.log(dani);
 
 // Nizovi su zapravo vrsta objekata koji imaju brojevne indekse.
@@ -87,7 +87,7 @@ console.log(daniStr);
 console.log(typeof daniStr);
 
 // obrnuti postupak
-let noviniz = daniStr.split(",");
+let noviniz = daniStr.split(',');
 console.log(noviniz);
 
 // 2.
@@ -95,7 +95,7 @@ const danistr2 = dani.join();
 console.log(danistr2);
 console.log(typeof danistr2);
 // Dobijanje stringa i stavljanje nekog znaka izmedju elemenata niza koji smo pretvorili u string
-const danistr3 = dani.join(": Murga je dobio sertifikat, ");
+const danistr3 = dani.join(': Murga je dobio sertifikat, ');
 console.log(danistr3);
 
 // pop() metoda brise poslednji element iz niza
