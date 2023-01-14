35854ec35139d2a174f1a867b2e423c382de88a7
DzedajMuharem <mujanovich100@gmail.com>
2023-01-14 09:57:14 +0100

    domaci

--- JS NIT/35.cas/app.js
+++ JS NIT/35.cas/app.js
@@ -45,6 +45,3 @@ console.log(numbers2);
 // Drugi deo. Napraviti jos jedan pomocni niz koji ce iz prvog dodatnog niza uzeti parne brojeve.
 // Treci deo. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
 // Funkcija na kraju treba da vrati dva napravljena pomocna niza.
-
-const DATA1 = [2, 26, 38, 75, 11, 29];
-const DATA2 = ['a', 'b', 'c', 'd', 'e'];
---
+++ JS NIT/35.cas/domaci.js
@@ -0,0 +1,41 @@
+// Domaci:
+// 1. Pomocu for petlje napraviti novi niz, koji ce sadrzati samo godine vece od 17.
+
+// 2.
+// Prvi deo. Napraviti pomocni niz koji ce sadrzati elemente od oba niza.
+// Drugi deo. Napraviti jos jedan pomocni niz koji ce iz prvog dodatnog niza uzeti parne brojeve.
+// Treci deo. U drugom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
+// Funkcija na kraju treba da vrati dva napravljena pomocna niza.
+
+const DATA1 = [2, 26, 38, 75, 11, 29];
+const DATA2 = ['a', 'b', 'c', 'd', 'e'];
+
+// 2.
+function zadatak2() {
+  const obaNiza = [...DATA1, ...DATA2];
+
+  function drugiNiz2(niz) {
+    let parniBrojevi = [];
+    for (let i of niz)
+      if (typeof i === typeof 123 && i % 2 === 0) {
+        parniBrojevi.push(i);
+      }
+    return parniBrojevi;
+  }
+  let drugiNiz = drugiNiz2(obaNiza);
+  drugiNiz.unshift(10, 20);
+
+  return `[ ${drugiNiz} ],
+  [ ${obaNiza} ]`;
+}
+console.log(zadatak2());
+// 1.
+
+function zadatak1(niz) {
+  let noviNiz = [];
+  for (let i of niz) {
+    if (i > 17) noviNiz.push(i);
+  }
+  return noviNiz;
+}
+console.log(zadatak1(DATA1));
