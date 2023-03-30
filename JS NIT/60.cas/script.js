177ef59a8465db539963cfa3f80e7cc8b514c0c8
DzedajMuharem <mujanovich100@gmail.com>
2023-03-30 17:57:55 +0200

    60.cas

--- JS NIT/60.cas/script.js
+++ JS NIT/60.cas/script.js
@@ -4,40 +4,42 @@ const slika = document.querySelector('.slika');
 const title = document.querySelector('.title');
 ///////////////////
 const getVideos = async () => {
-  const response = await fetch(
-    'https://services.brid.tv/services/get/latest/26456/0/1/25/0.json'
-  );
-  const data = await response.json();
-  console.log(data);
-  const playerId = data.Player.id;
-  console.log(playerId);
-  const videos = data.Video;
-  console.log(videos);
-  let time = function (vrijeme) {
-    let sati;
-    let minuti;
-    let sekunde;
-    if (vrijeme < 60) {
-      return `00:00:${vrijeme < 10 ? `0${vrijeme}` : `${vrijeme}`}`;
-    }
-    if (vrijeme > 60 && vrijeme < 3600) {
-      minuti = Math.floor(vrijeme / 60);
-      sekunde = vrijeme - Math.floor(vrijeme / 60) * 60;
-      return `00:${minuti < 10 ? `0${minuti}` : `${minuti}`}:${
-        sekunde < 10 ? `0${sekunde}` : `${sekunde}`
-      }`;
-    } else {
-      sati = Math.floor(vrijeme / 3600);
-      minuti = Math.floor(vrijeme / 60) - Math.floor(vrijeme / 3600) * 3600;
-      sekunde = vrijeme - Math.floor(vrijeme / 60) * 60;
-      return `${sati < 10 ? `0${sati}` : `${sati}`}:${
-        minuti < 10 ? `0${minuti}` : `${minuti}`
-      }:${sekunde < 10 ? `0${sekunde}` : `${sekunde}`}`;
-    }
-  };
+  try {
+    const response = await fetch(
+      'https://services.brid.tv/services/get/latest/26456/0/1/25/0.json'
+    );
+    const data = await response.json();
+    console.log(data);
 
-  videos.forEach(element => {
-    lista.innerHTML += `<li>
+    const playerId = data.Player.id;
+    console.log(playerId);
+    const videos = data.Video;
+    console.log(videos);
+    let time = function (vrijeme) {
+      let sati;
+      let minuti;
+      let sekunde;
+      if (vrijeme < 60) {
+        return `00:00:${vrijeme < 10 ? `0${vrijeme}` : `${vrijeme}`}`;
+      }
+      if (vrijeme > 60 && vrijeme < 3600) {
+        minuti = Math.floor(vrijeme / 60);
+        sekunde = vrijeme - Math.floor(vrijeme / 60) * 60;
+        return `00:${minuti < 10 ? `0${minuti}` : `${minuti}`}:${
+          sekunde < 10 ? `0${sekunde}` : `${sekunde}`
+        }`;
+      } else {
+        sati = Math.floor(vrijeme / 3600);
+        minuti = Math.floor(vrijeme / 60) - Math.floor(vrijeme / 3600) * 3600;
+        sekunde = vrijeme - Math.floor(vrijeme / 60) * 60;
+        return `${sati < 10 ? `0${sati}` : `${sati}`}:${
+          minuti < 10 ? `0${minuti}` : `${minuti}`
+        }:${sekunde < 10 ? `0${sekunde}` : `${sekunde}`}`;
+      }
+    };
+
+    videos.forEach(element => {
+      lista.innerHTML += `<li>
     <div class="kartica">
       <img
       onclick="loadVideo(this)"
@@ -48,31 +50,37 @@ const getVideos = async () => {
       />
       <div class="donjideo" >
        <p class="title"  onclick="loadVideo(this)" id="${element.id}">${
-      element.name
-    }</p>
+        element.name
+      }</p>
           <p class="duration">${time(element.duration)}</p>
       </div>
     </div>
   </li>`;
-  });
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 getVideos();
 
-let player = $bp('DIV_ID', {
-  id: '26456',
-  width: '640',
-  height: '480',
-  video: `1272195`,
-});
+// let player = $bp('DIV_ID', {
+//   id: '26456',
+//   width: '640',
+//   height: '480',
+//   video: '',
+// });
+
+let player = null;
 
 function loadVideo(element) {
-  // let nesto = element.parentElement;
-  // console.log(nesto.parentElement.id);
-  console.log(element.id);
-  $bp('DIV_ID', {
+  if (player) {
+    player.stop();
+  }
+  player = $bp('DIV_ID', {
     id: '26456',
     width: '640',
     height: '480',
     video: `${element.id}`,
   });
+  player.play();
 }
